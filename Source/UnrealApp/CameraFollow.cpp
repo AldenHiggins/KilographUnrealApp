d3535da763265bfc89250f30b8ea23055c45b332
Alden Higgins <johnaldenhiggins@gmail.com>
2015-12-23 15:10:56 -0800

    Fixed crash because the crosshair texture couldn't be found and got rid of camera follow (going to add as a blueprintable script)

--- Source/UnrealApp/CameraFollow.cpp
+++
@@ -1,2 +0,0 @@
-#include "CameraFollow.h"
-
--- Source/UnrealApp/UnrealAppHUD.cpp
+++ Source/UnrealApp/UnrealAppHUD.cpp
@@ -9,7 +9,7 @@
 AUnrealAppHUD::AUnrealAppHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshiarTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshiarTexObj(TEXT("/Game/FirstPerson/Textures/UI/FirstPersonCrosshair"));
 	CrosshairTex = CrosshiarTexObj.Object;
 }
 
