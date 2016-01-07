4fe7f4a3aafc3ed3ca92b22233da60f734a09e60
U-KILOGRAPH\AldenHiggins <AldenHiggins@MERCURIUS-PC.KILOGRAPH.local>
2016-01-06 16:08:40 -0800

    Cleared out repo

--- Source/UnrealApp/UnrealApp.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
-
-#include "UnrealApp.h"
-
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealApp, "UnrealApp" );
- 
-//General Log
-DEFINE_LOG_CATEGORY(Kilograph);
